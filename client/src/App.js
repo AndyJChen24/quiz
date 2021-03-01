b9771d562ec2d9048d24511e1b435518bddb9261
Andy <ajiajiechen@gmail.com>
2021-02-28 21:30:54 -0800

    put questions from api call to state hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Api from './utils/Api';
+import React, {useState} from 'react';
 
 function App() {
+  const [questions, setQuestions] = useState('');
 
+  function getQuestions(event){
+    event.preventDefault();
+    Api.getQuestions()
+      .then((res)=>{
+        setQuestions(res.data)
+      })
+  }
   return (
     <div className="App">
       <p>Quiz App</p>
-      <button onClick={Api.getQuestions}>Button</button>
+      <button onClick={getQuestions}>Button</button>
     </div>
   );
 }
