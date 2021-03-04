f08a668a72506e5d9d524b3a4219f89b3d872a95
Andy <ajiajiechen@gmail.com>
2021-03-04 14:38:53 -0800

    added radio button to answers and fixed problem with radio button with empty answers using css and class attribute

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       .then((res)=>{
         // put the data in state
         setQuestions(res.data)
-        console.log(questions)
+        //console.log(questions)
       })
   }
   return (
@@ -25,13 +25,13 @@ function App() {
       {questions && questions.map((question)=> (
         <div key={question.id}>
           <h3>{question.question}</h3>
-          {/* if there are answers and there is an answer a - f display it */}
-          <p> {question.answers && question.answers.answer_a}</p>
-          <p> {question.answers && question.answers.answer_b}</p>
-          <p> {question.answers && question.answers.answer_c}</p>
-          <p> {question.answers && question.answers.answer_d}</p>
-          <p> {question.answers && question.answers.answer_e}</p>
-          <p> {question.answers && question.answers.answer_f}</p>
+          {/* create a button for each answer avaible. The the radio button is created no matter so to solve this problem of empty answer with radio button i created a class that forces the class name to be null if there is no answer*/}
+          <input type="radio" name="answer" class={``+question.answers.answer_a} value={question.answers.answer_a}/> {question.answers && question.answers.answer_a} 
+          <input type="radio" name="answer" class={``+question.answers.answer_b} value={question.answers.answer_b}/> {question.answers && question.answers.answer_b}
+          <input type="radio" name="answer" class={``+question.answers.answer_c} value={question.answers.answer_c}/> {question.answers && question.answers.answer_c}
+          <input type="radio" name="answer" class={``+question.answers.answer_d} value={question.answers.answer_d}/> {question.answers && question.answers.answer_d}
+          <input type="radio" name="answer" class={``+question.answers.answer_e} value={question.answers.answer_e}/> {question.answers && question.answers.answer_e}
+          <input type="radio" name="answer" class={``+question.answers.answer_f} value={question.answers.answer_f}/> {question.answers && question.answers.answer_f}
 
         </div>
         
