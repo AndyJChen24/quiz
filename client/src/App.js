5f5e71d20f6589dff7fce8382529c6845bf261a0
Andy <ajiajiechen@gmail.com>
2021-03-04 15:31:04 -0800

    fixed problem where only one radio button can be click at one time due to misplacement of form tag

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       .then((res)=>{
         // put the data in state
         setQuestions(res.data)
-        //console.log(questions)
+        console.log(questions)
       })
   }
   return (
@@ -20,23 +20,24 @@ function App() {
       <p>Quiz App</p>
       {/* on button press get random questions */}
       <button onClick={getQuestions}>Button</button>
-      <form>
+      
       {/* if there are questions, map out all the questions in the array of objects */}
       {questions && questions.map((question)=> (
         <div key={question.id}>
           <h3>{question.question}</h3>
-          {/* create a button for each answer avaible. The the radio button is created no matter so to solve this problem of empty answer with radio button i created a class that forces the class name to be null if there is no answer*/}
-          <input type="radio" name="answer" class={``+question.answers.answer_a} value={question.answers.answer_a}/> {question.answers && question.answers.answer_a} 
-          <input type="radio" name="answer" class={``+question.answers.answer_b} value={question.answers.answer_b}/> {question.answers && question.answers.answer_b}
-          <input type="radio" name="answer" class={``+question.answers.answer_c} value={question.answers.answer_c}/> {question.answers && question.answers.answer_c}
-          <input type="radio" name="answer" class={``+question.answers.answer_d} value={question.answers.answer_d}/> {question.answers && question.answers.answer_d}
-          <input type="radio" name="answer" class={``+question.answers.answer_e} value={question.answers.answer_e}/> {question.answers && question.answers.answer_e}
-          <input type="radio" name="answer" class={``+question.answers.answer_f} value={question.answers.answer_f}/> {question.answers && question.answers.answer_f}
-
+          <form>
+            {/* create a button for each answer avaible. The the radio button is created no matter what, so to solve this problem of empty answer with radio button. I created a class that forces the class name to be null if there is no answer*/}
+            <input type="radio" name="answer" className={``+question.answers.answer_a} value={``+question.answers.answer_a}/> {question.answers && question.answers.answer_a} 
+            <input type="radio" name="answer" className={``+question.answers.answer_b} value={``+question.answers.answer_b}/> {question.answers && question.answers.answer_b}
+            <input type="radio" name="answer" className={``+question.answers.answer_c} value={``+question.answers.answer_c}/> {question.answers && question.answers.answer_c}
+            <input type="radio" name="answer" className={``+question.answers.answer_d} value={``+question.answers.answer_d}/> {question.answers && question.answers.answer_d}
+            <input type="radio" name="answer" className={``+question.answers.answer_e} value={``+question.answers.answer_e}/> {question.answers && question.answers.answer_e}
+            <input type="radio" name="answer" className={``+question.answers.answer_f} value={``+question.answers.answer_f}/> {question.answers && question.answers.answer_f}
+          </form>
         </div>
         
       ))}
-      </form>
+      
     </div>
   );
 }
