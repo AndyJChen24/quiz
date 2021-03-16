a0c5a264307dff91bf6fa832798e394a8e3a5048
Andy <ajiajiechen@gmail.com>
2021-03-16 13:55:40 -0700

    make new state for easier comparison later on when user submit answers

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,11 @@ import Api from './utils/Api';
 import React, {useState} from 'react';
 
 function App() {
-  const [questions, setQuestions] = useState('');
+  const [questions, setQuestions] = useState([]);
+  const [answerKey, setAnswers] = useState({})
+  const [userAnswerKey, setUserAnswers] = useState({})
+  const [currentAnswer, setCurrentAnswer] = useState({})
+  const [data, setData] = useState()
 
   // get question from api call
   function getQuestions(event){
@@ -11,9 +15,23 @@ function App() {
     Api.getQuestions()
       .then((res)=>{
         // put the data in state
-        setQuestions(res.data)
+        setData(res.data)
+        
+        res.data.forEach((element)=>{
+          
+          setQuestions(questions + element.question)
+        })
         console.log(res.data)
+        
       })
+  }
+
+  function recordAnswers(event){
+    const newAnswerKey = answerKey
+    answerKey.push(event.target.value)
+    setAnswers(answerKey + event.target.value)
+    console.log(event.target.value)
+    console.log(answerKey)
   }
   return (
     <div className="App">
@@ -22,17 +40,16 @@ function App() {
       <button onClick={getQuestions}>Button</button>
       
       {/* if there are questions, map out all the questions in the array of objects */}
-      {questions && questions.map((question)=> (
+      {data && data.map((question)=> (
         <div key={question.id}>
           <h3>{question.question}</h3>
           <form>
-            {/* create a button for each answer avaible. The the radio button is created no matter what, so to solve this problem of empty answer with radio button. I created a class that forces the class name to be null if there is no answer*/}
-            <input type="radio" name="answer" className={``+question.answers.answer_a} value={``+question.answers.answer_a}/> {question.answers && question.answers.answer_a} 
-            <input type="radio" name="answer" className={``+question.answers.answer_b} value={``+question.answers.answer_b}/> {question.answers && question.answers.answer_b}
-            <input type="radio" name="answer" className={``+question.answers.answer_c} value={``+question.answers.answer_c}/> {question.answers && question.answers.answer_c}
-            <input type="radio" name="answer" className={``+question.answers.answer_d} value={``+question.answers.answer_d}/> {question.answers && question.answers.answer_d}
-            <input type="radio" name="answer" className={``+question.answers.answer_e} value={``+question.answers.answer_e}/> {question.answers && question.answers.answer_e}
-            <input type="radio" name="answer" className={``+question.answers.answer_f} value={``+question.answers.answer_f}/> {question.answers && question.answers.answer_f}
+
+            {question.answerArray && question.answerArray.map((answer, index)=> (
+              <div key={index}>
+                <input name={question.question} type="radio" value={answer.answerValue} onChange={recordAnswers}/>{answer.answerValue}
+              </div>
+            ))}
           </form>
         </div>
         
